23a79ead203ee1c5216c1f27cec824e2857a78d1
Nick Mitrokhin <nikolai.mitrokhin@devexpress.com>
2017-04-26 20:12:55 +0300

    16.2.6 hotfix (v16.2.7-pre-17116)

--- js/vectormap-utils/dx.vectormaputils.node.js
+++ js/vectormap-utils/dx.vectormaputils.node.js
@@ -1,7 +1,7 @@
 /*!
  * DevExtreme (dx.vectormaputils.node.js)
- * Version: 16.2.6 (build 17111)
- * Build date: Fri Apr 21 2017
+ * Version: 16.2.6 (build 17116)
+ * Build date: Wed Apr 26 2017
  *
  * Copyright (c) 2012 - 2017 Developer Express Inc. ALL RIGHTS RESERVED
  * Read about DevExtreme licensing here: https://js.devexpress.com/Licensing/
