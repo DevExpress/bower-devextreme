3646e2e22139f04a9081853ac7ed913c13f1b03b
BingoRUS <bingofirst@gmail.com>
2017-08-02 18:09:42 +0300

    Update to 17.1.5

--- js/localization/dx.all.de.js
+++ js/localization/dx.all.de.js
@@ -1,7 +1,7 @@
 /*!
 * DevExtreme (dx.all.de.js)
-* Version: 17.1.4 (build 17208)
-* Build date: Thu Jul 27 2017
+* Version: 17.1.5
+* Build date: Tue Aug 01 2017
 *
 * Copyright (c) 2012 - 2017 Developer Express Inc. ALL RIGHTS RESERVED
 * Read about DevExtreme licensing here: https://js.devexpress.com/Licensing/
