b9a2ad6c46728bcab9cc8158fbaa3fe7e9c02f25
dxrobot <DXGitHubRobot@devexpress.com>
2017-10-13 11:45:11 +0300

    17.1.7 hotfix (v17.1.8-pre-17286)

--- js/dx.aspnet.mvc.js
+++ js/dx.aspnet.mvc.js
@@ -1,7 +1,7 @@
 /*!
 * DevExtreme (dx.aspnet.mvc.js)
-* Version: 17.1.7 (build 17284)
-* Build date: Wed Oct 11 2017
+* Version: 17.1.7 (build 17286)
+* Build date: Fri Oct 13 2017
 *
 * Copyright (c) 2012 - 2017 Developer Express Inc. ALL RIGHTS RESERVED
 * Read about DevExtreme licensing here: https://js.devexpress.com/Licensing/
