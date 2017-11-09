517b622013d73688a781c216e4332bc84bee45b3
babich-a <alexey.babych@gmail.com>
2017-11-09 21:06:40 +0300

    Update to 17.1.8

--- ts/dx.all.d.ts
+++ ts/dx.all.d.ts
@@ -1,7 +1,7 @@
 /*!
 * DevExtreme (dx.all.d.ts)
-* Version: 17.1.7 (build 17299)
-* Build date: Thu Oct 26 2017
+* Version: 17.1.8
+* Build date: Tue Nov 07 2017
 *
 * Copyright (c) 2012 - 2017 Developer Express Inc. ALL RIGHTS RESERVED
 * Read about DevExtreme licensing here: https://js.devexpress.com/Licensing/
