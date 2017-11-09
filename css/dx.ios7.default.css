308fa7855df5d7d6ed2503f3700f256ab7aebfc0
babich-a <alexey.babych@gmail.com>
2017-11-09 21:03:31 +0300

    Update to 16.1.14

--- css/dx.ios7.default.css
+++ css/dx.ios7.default.css
@@ -1,7 +1,7 @@
 /*! 
 * DevExtreme
-* Version: 16.1.13
-* Build date: Jul 28, 2017
+* Version: 16.1.14
+* Build date: Nov 3, 2017
 *
 * Copyright (c) 2012 - 2017 Developer Express Inc. ALL RIGHTS RESERVED
 * EULA: https://www.devexpress.com/Support/EULAs/DevExtreme.xml
--- js/vectormap-utils/dx.vectormaputils.js
+++ js/vectormap-utils/dx.vectormaputils.js
@@ -1,7 +1,7 @@
 /*! 
 * DevExtreme (dx.vectormaputils.js)
-* Version: 16.1.13
-* Build date: Fri Jul 28 2017
+* Version: 16.1.14
+* Build date: Fri Nov 03 2017
 *
 * Copyright (c) 2012 - 2017 Developer Express Inc. ALL RIGHTS RESERVED
 * EULA: https://www.devexpress.com/Support/EULAs/DevExtreme.xml
