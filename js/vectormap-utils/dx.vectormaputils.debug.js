77eaa994dba7d18a33e0fb80655723174a0969f0
groshenkovamarina <magroshenkova@gmail.com>
2017-12-20 18:15:59 +0300

    Update to 17.1.9

--- js/vectormap-utils/dx.vectormaputils.debug.js
+++ js/vectormap-utils/dx.vectormaputils.debug.js
@@ -1,7 +1,7 @@
 /*!
  * DevExtreme (dx.vectormaputils.debug.js)
- * Version: 17.1.8 (build 17338)
- * Build date: Mon Dec 04 2017
+ * Version: 17.1.9
+ * Build date: Mon Dec 18 2017
  *
  * Copyright (c) 2012 - 2017 Developer Express Inc. ALL RIGHTS RESERVED
  * Read about DevExtreme licensing here: https://js.devexpress.com/Licensing/
