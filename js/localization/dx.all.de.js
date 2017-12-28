2a746f997e9b921877a29e5742ec9cf89570a691
dxrobot <DXGitHubRobot@devexpress.com>
2017-12-28 22:12:31 +0300

    17.2.4 hotfix (v17.2.5-pre-17362)

--- js/localization/dx.all.de.js
+++ js/localization/dx.all.de.js
@@ -1,7 +1,7 @@
 /*!
 * DevExtreme (dx.all.de.js)
-* Version: 17.2.4 (build 17356)
-* Build date: Fri Dec 22 2017
+* Version: 17.2.4 (build 17362)
+* Build date: Thu Dec 28 2017
 *
 * Copyright (c) 2012 - 2017 Developer Express Inc. ALL RIGHTS RESERVED
 * Read about DevExtreme licensing here: https://js.devexpress.com/Licensing/
