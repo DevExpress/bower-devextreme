ceb5460f7e4f10dc30261bd483ea02b053f85311
BingoRUS <bingofirst@gmail.com>
2018-09-05 16:17:47 +0300

    Update to 18.1.6

--- js/vectormap-utils/dx.vectormaputils.debug.js
+++ js/vectormap-utils/dx.vectormaputils.debug.js
@@ -1,7 +1,7 @@
 /*!
  * DevExtreme (dx.vectormaputils.debug.js)
- * Version: 18.1.5 (build 18234)
- * Build date: Wed Aug 22 2018
+ * Version: 18.1.6
+ * Build date: Mon Sep 03 2018
  *
  * Copyright (c) 2012 - 2018 Developer Express Inc. ALL RIGHTS RESERVED
  * Read about DevExtreme licensing here: https://js.devexpress.com/Licensing/
