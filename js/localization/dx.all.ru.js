cc75b266ec017fbb931d9ba6134d79a482bb06db
dxrobot <DXGitHubRobot@devexpress.com>
2018-10-25 17:06:05 +0300

    17.2.10 hotfix (v17.2.11-pre-18298)

--- js/localization/dx.all.ru.js
+++ js/localization/dx.all.ru.js
@@ -1,7 +1,7 @@
 /*!
 * DevExtreme (dx.all.ru.js)
-* Version: 17.2.10 (build 18291)
-* Build date: Thu Oct 18 2018
+* Version: 17.2.10 (build 18298)
+* Build date: Thu Oct 25 2018
 *
 * Copyright (c) 2012 - 2018 Developer Express Inc. ALL RIGHTS RESERVED
 * Read about DevExtreme licensing here: https://js.devexpress.com/Licensing/
