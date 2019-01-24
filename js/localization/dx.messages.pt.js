02de5409d1b3ca07cb2253aee54796d7492b40c8
babich-a <alexey.babych@gmail.com>
2019-01-24 15:12:01 +0300

    Update to 18.2.5

--- js/localization/dx.messages.pt.js
+++ js/localization/dx.messages.pt.js
@@ -1,7 +1,7 @@
 /*!
 * DevExtreme (dx.messages.pt.js)
-* Version: 18.2.4 (build 19010)
-* Build date: Thu Jan 10 2019
+* Version: 18.2.5
+* Build date: Wed Jan 23 2019
 *
 * Copyright (c) 2012 - 2019 Developer Express Inc. ALL RIGHTS RESERVED
 * Read about DevExtreme licensing here: https://js.devexpress.com/Licensing/
