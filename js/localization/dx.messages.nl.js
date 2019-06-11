f09ce2eae41b12050f85d11e0b3eaa52b8a64c69
DX Robot <DXGitHubRobot@devexpress.com>
2019-06-11 12:42:52 +0300

    Update to 18.2.9

--- js/localization/dx.messages.nl.js
+++ js/localization/dx.messages.nl.js
@@ -1,7 +1,7 @@
 /*!
 * DevExtreme (dx.messages.nl.js)
-* Version: 18.2.8 (build 19149)
-* Build date: Wed May 29 2019
+* Version: 18.2.9
+* Build date: Mon Jun 10 2019
 *
 * Copyright (c) 2012 - 2019 Developer Express Inc. ALL RIGHTS RESERVED
 * Read about DevExtreme licensing here: https://js.devexpress.com/Licensing/
