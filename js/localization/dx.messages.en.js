951da8a440969afcc0743fc59ff1d6ed147c7bfe
dxrobot <DXGitHubRobot@devexpress.com>
2019-10-31 11:05:14 +0300

    18.2.10 hotfix (v18.2.11-pre-19304)

--- js/localization/dx.messages.en.js
+++ js/localization/dx.messages.en.js
@@ -1,7 +1,7 @@
 /*!
 * DevExtreme (dx.messages.en.js)
-* Version: 18.2.10 (build 19289)
-* Build date: Wed Oct 16 2019
+* Version: 18.2.10 (build 19304)
+* Build date: Thu Oct 31 2019
 *
 * Copyright (c) 2012 - 2019 Developer Express Inc. ALL RIGHTS RESERVED
 * Read about DevExtreme licensing here: https://js.devexpress.com/Licensing/
