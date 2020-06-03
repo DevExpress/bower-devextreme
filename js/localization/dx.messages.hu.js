6074ea7672adc431c6ac50d624077a8ddade135e
dxrobot <DXGitHubRobot@devexpress.com>
2020-06-03 21:04:34 +0300

    19.1.11 hotfix (v19.1.12-build-20155-0314)

--- js/localization/dx.messages.hu.js
+++ js/localization/dx.messages.hu.js
@@ -1,7 +1,7 @@
 /*!
 * DevExtreme (dx.messages.hu.js)
-* Version: 19.1.11
-* Build date: Fri May 15 2020
+* Version: 19.1.11 (build 20155-0314)
+* Build date: Wed Jun 03 2020
 *
 * Copyright (c) 2012 - 2020 Developer Express Inc. ALL RIGHTS RESERVED
 * Read about DevExtreme licensing here: https://js.devexpress.com/Licensing/
