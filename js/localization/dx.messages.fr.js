4c547ce7fe006ce2a625d7241985fff9130f259c
DX Robot <DXGitHubRobot@devexpress.com>
2020-06-04 10:57:39 +0300

    Update to 18.2.13

--- js/localization/dx.messages.fr.js
+++ js/localization/dx.messages.fr.js
@@ -1,7 +1,7 @@
 /*!
 * DevExtreme (dx.messages.fr.js)
-* Version: 18.2.12
-* Build date: Tue Mar 24 2020
+* Version: 18.2.13
+* Build date: Wed May 27 2020
 *
 * Copyright (c) 2012 - 2020 Developer Express Inc. ALL RIGHTS RESERVED
 * Read about DevExtreme licensing here: https://js.devexpress.com/Licensing/
--- ts/dx.all.d.ts
+++ ts/dx.all.d.ts
@@ -1,7 +1,7 @@
 /*!
 * DevExtreme (dx.all.d.ts)
-* Version: 18.2.12
-* Build date: Tue Mar 24 2020
+* Version: 18.2.13
+* Build date: Wed May 27 2020
 *
 * Copyright (c) 2012 - 2020 Developer Express Inc. ALL RIGHTS RESERVED
 * Read about DevExtreme licensing here: https://js.devexpress.com/Licensing/
