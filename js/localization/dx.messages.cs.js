5fa1fef7efb418d886ba29b98b9fe5bcaf7dc02f
dxrobot <DXGitHubRobot@devexpress.com>
2020-07-16 10:43:42 +0300

    18.2.13 hotfix (v18.2.14-pre-20198)

--- js/localization/dx.messages.cs.js
+++ js/localization/dx.messages.cs.js
@@ -1,7 +1,7 @@
 /*!
 * DevExtreme (dx.messages.cs.js)
-* Version: 18.2.13
-* Build date: Wed May 27 2020
+* Version: 18.2.13 (build 20198)
+* Build date: Thu Jul 16 2020
 *
 * Copyright (c) 2012 - 2020 Developer Express Inc. ALL RIGHTS RESERVED
 * Read about DevExtreme licensing here: https://js.devexpress.com/Licensing/
