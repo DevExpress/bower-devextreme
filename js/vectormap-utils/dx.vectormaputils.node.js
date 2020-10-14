fc072da7ff03591ea1fff161df9343d2ae0c7203
DX Robot <DXGitHubRobot@devexpress.com>
2020-10-14 13:54:01 +0300

    Update to 18.2.14

--- js/vectormap-utils/dx.vectormaputils.node.js
+++ js/vectormap-utils/dx.vectormaputils.node.js
@@ -1,7 +1,7 @@
 /*!
  * DevExtreme (dx.vectormaputils.node.js)
- * Version: 18.2.13 (build 20198)
- * Build date: Thu Jul 16 2020
+ * Version: 18.2.14
+ * Build date: Fri Oct 09 2020
  *
  * Copyright (c) 2012 - 2020 Developer Express Inc. ALL RIGHTS RESERVED
  * Read about DevExtreme licensing here: https://js.devexpress.com/Licensing/
