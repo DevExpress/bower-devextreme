124cecfd2991eead840a95206f1a18f64e23d747
DX Robot <DXGitHubRobot@devexpress.com>
2021-04-29 17:49:03 +0300

    Update to 19.1.13

--- js/localization/dx.messages.it.js
+++ js/localization/dx.messages.it.js
@@ -1,9 +1,9 @@
 /*!
 * DevExtreme (dx.messages.it.js)
-* Version: 19.1.12
-* Build date: Fri Oct 09 2020
+* Version: 19.1.13
+* Build date: Fri Apr 23 2021
 *
-* Copyright (c) 2012 - 2020 Developer Express Inc. ALL RIGHTS RESERVED
+* Copyright (c) 2012 - 2021 Developer Express Inc. ALL RIGHTS RESERVED
 * Read about DevExtreme licensing here: https://js.devexpress.com/Licensing/
 */
 "use strict";
