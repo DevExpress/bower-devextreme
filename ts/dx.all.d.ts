80b7a7f95d9941e42c0f60743ee82d30866d897e
DX Robot <DXGitHubRobot@devexpress.com>
2021-12-09 09:44:34 +0300

    Update to 19.1.14

--- ts/dx.all.d.ts
+++ ts/dx.all.d.ts
@@ -1,7 +1,7 @@
 /*!
 * DevExtreme (dx.all.d.ts)
-* Version: 19.1.13 (build 21160-0311)
-* Build date: Wed Jun 09 2021
+* Version: 19.1.14
+* Build date: Fri Dec 03 2021
 *
 * Copyright (c) 2012 - 2021 Developer Express Inc. ALL RIGHTS RESERVED
 * Read about DevExtreme licensing here: https://js.devexpress.com/Licensing/
